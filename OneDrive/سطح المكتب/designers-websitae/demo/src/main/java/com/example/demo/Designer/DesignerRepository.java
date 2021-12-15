2dc9b167ed53b67910a3fae7254a239d08ee0286
thuriabdullah <thiii302081@gmail.com>
2021-12-15 07:50:25 +0300

    add Register Component and redux user reducer and action create store

--- OneDrive/سطح المكتب/designers-websitae/demo/src/main/java/com/example/demo/Designer/DesignerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Designer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DesignerRepository extends JpaRepository <Designer,Long>{
-}
--- OneDrive/سطح المكتب/designers-websitae/demo/src/main/java/com/example/demo/Utility/UtilityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Utility;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UtilityRepository extends JpaRepository <Utility,Long> {
-
-}
